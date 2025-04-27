276307c062b6d098b8b75763fb318a3fa46127ee
Pravin M <pravinm.23aid@kongu.edu>
2025-04-27 15:15:53 +0530

    Time: 0 ms (100%), Space: 40.7 MB (53.89%) - LeetHub

--- 2748-calculate-delayed-arrival-time/2748-calculate-delayed-arrival-time.java
+++ 2748-calculate-delayed-arrival-time/2748-calculate-delayed-arrival-time.java
@@ -1,9 +1,6 @@
 class Solution {
     public int findDelayedArrivalTime(int arrivalTime, int delayedTime) {
         int p=arrivalTime+delayedTime;
-        if(p>=24){
-            return p%24;
-        }
-        return p;
+        return p%24;
     }
 }
