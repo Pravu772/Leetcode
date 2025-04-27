9b67b1d4cf63b6066c34438a4c1f280bde5e4ab7
Pravin M <pravinm.23aid@kongu.edu>
2025-04-27 20:21:44 +0530

    Time: 0 ms (100%), Space: 40.5 MB (85.83%) - LeetHub

--- 0202-happy-number/0202-happy-number.java
+++ 0202-happy-number/0202-happy-number.java
@@ -1,17 +1,14 @@
 class Solution {
     public boolean isHappy(int n) {
-        if(n==1 || n==7) return true;
-        else if(n<10) return false;
-                
-        else{
+        while(n!=1 && n!=4){
             int sum=0;
             while(n>0){
-                int temp=n%10;
-                sum+=temp*temp;
+                sum+=(n%10)*(n%10);
                 n/=10;
             }
-            return isHappy(sum);
+            n=sum;
         }
+        return n==1;
         
     }
 }
